eb923cd22cacb49afb5bfd49ce320925f365bf02
gustavoresendes <gustavopenna2001@gmail.com>
2023-03-31 13:22:41 -0300

    fixed http error

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const KEY = "9a7e620a163b4a42b99161709230401";
 
 const fetchData = async (city) => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
   const fetchResponse = await fetch(url);
   const data = await fetchResponse.json();
